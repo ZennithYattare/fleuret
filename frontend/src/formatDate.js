7d8705d9ce095b8c24223e6501d51851d458e84f
ZennithYattare <luis.espera.0758@gmail.com>
2022-04-26 21:45:42 +0800

    Changed date format to fix sorting issue

--- frontend/src/formatDate.js
+++ frontend/src/formatDate.js
@@ -1,9 +1,16 @@
 export const formatDate = (string) => {
+    
+    var newDateOptions = {
+        year: "numeric",
+        month: "short",
+        day: "2-digit"
+    }
+
     if (string === "") {
         return "-";
     } else {
         let date = new Date(string);
-        var month = date.toLocaleString("default", { month: "short" });
-        return month + ". " + date.getDate() + ", " + date.getFullYear();
+        var month = date.toLocaleString("en-GB", newDateOptions);
+        return month;
     }
 };
